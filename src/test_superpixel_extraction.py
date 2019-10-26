f2b910e836989db67f168b27c12719d2b2019960
Gerry Chen <gdc9@duke.edu>
2019-10-26 10:25:40 -0400

    add files

---
+++ src/test_superpixel_extraction.py
@@ -0,0 +1,13 @@
+"""
+tests superpixel extractions
+"""
+
+import unittest
+
+class TestSuperpixelExtraction(unittest.TestCase):
+
+    def test_superpixel_extraction(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
