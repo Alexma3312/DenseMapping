28900c6474df09581a12b5c7a61f854f27b4166e
Shicong Ma <alexma3312@gmail.com>
2019-10-26 12:25:20 -0400

    Add surfel generation and its test file.

---
+++ src/test_surfel_generation.py
@@ -0,0 +1,15 @@
+"""Test Surfel Generation."""
+import unittest
+
+from surfel_generation import initialize_surfels
+
+
+class TestSurfelGeneration(unittest.TestCase):
+    def test_initialize_surfels(self):
+        """Initialize Surfels."""
+        # initialize_surfels
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
