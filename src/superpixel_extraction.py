0515bf299e23fd68d3286d78ce145fb754fade94
Gerry Chen <gdc9@duke.edu>
2019-10-26 11:02:24 -0400

    function declarations

--- src/superpixel_extraction.py
+++ src/superpixel_extraction.py
@@ -2,3 +2,20 @@
 Extract superpixels
 """
 
+def extract_superpixels(image):
+    pass
+
+def init_seeds(image):
+    pass
+
+def assign_pixels(image):
+    pass
+
+def update_seeds(image):
+    pass
+
+def calc_norms(image):
+    pass
+
+if __name__ == "__main__":
+    pass
--- src/test_superpixel_extraction.py
+++ src/test_superpixel_extraction.py
@@ -6,7 +6,19 @@ import unittest
 
 class TestSuperpixelExtraction(unittest.TestCase):
 
-    def test_superpixel_extraction(self):
+    def test_extract_superpixels(self):
+        pass
+
+    def test_init_seeds(self):
+        pass
+
+    def test_assign_pixels(self):
+        pass
+
+    def test_update_seeds(self):
+        pass
+
+    def test_calc_norms(self):
         pass
 
 if __name__ == '__main__':
