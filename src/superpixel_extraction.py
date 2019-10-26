2f1c6d897322c84040dd34461e4affdd8e42672f
Gerry Chen <gdc9@duke.edu>
2019-10-26 14:37:50 -0400

    superpixel class

--- src/superpixel_extraction.py
+++ src/superpixel_extraction.py
@@ -4,67 +4,80 @@ Extract superpixels
 
 from superpixel_seed import SuperpixelSeed
 
-def extract_superpixels(image, depth):
-    """Extracts superpixels from an RGB image and depth image
-    Arguments:
-        image:  input image (nxmx3)
-        depth:  depth image (nxm)
-    Returns:
-        supepixels: list of SuperpixelSeed
-    """
-    return None
+class SuperpixelExtraction():
+    
+    def __init__(self, image, depth, camera_parameters, sp_size=8):
+        self.image = image
+        self.depth = depth
+        (self.im_width, self.im_height) = image.shape
+        self.fx = camera_parameters['fx']
+        self.fy = camera_parameters['fy']
+        self.cx = camera_parameters['cx']
+        self.cy = camera_parameters['cy']
+        self.sp_size = sp_size
 
-def init_seeds(image, depth, sp_size=8):
-    """Initializes the centers for the superpixels
-    Arguments:
-        image:  input image (nxmx3)
-        depth:  depth image (nxm)
-        sp_size: initial size of the superpixel grid
-    Returns:
-        superpixels:    list of SuperpixelSeed, has correct x, y.  mean_depth
-            and mean_intensity are initialized to the value of the center pixel,
-            and the remaining properties are initialized to 0.
-    """
-    return None
+    def extract_superpixels(self):
+        """Extracts superpixels from an RGB image and depth image
+        Member dependencies:
+            image, depth
+        Arguments:
+            None
+        Returns:
+            supepixels: list of SuperpixelSeed
+        """
+        return None
 
-def assign_pixels(image, depth, superpixels):
-    """Assigns each pixel in an image to a superpixel seed
-    Arguments:
-        image:  input image (nxmx3)
-        depth:  depth image (nxm)
-        superpixels:    list of SuperpixelSeed
-    Returns:
-        pixels:    nxm array of indices, where nxm is the size of the image and
-            each element in the array represents the index of the superpixel
-            which that pixel in the image is assigned to
-    """
-    return None
+    def init_seeds(self):
+        """Initializes the centers for the superpixels
+        Member dependencies:
+            image, depth:  depth image, sp_size
+        Arguments:
+            None
+        Returns:
+            superpixels:    list of SuperpixelSeed, has correct x, y.  mean_depth
+                and mean_intensity are initialized to the value of the center pixel,
+                and the remaining properties are initialized to 0.
+        """
+        return None
 
-def update_seeds(image, depth, pixels, superpixels):
-    """Updates the locations of the superpixel seeds
-    Arguments:
-        image:  input image (nxmx3)
-        depth:  depth image (nxm)
-        pixels: array of indices (nxm) which assigns each pixel to the index in 
-            `superpixels` of the superpixel under which this pixel falls
-        superpixels:    list of SuperpixelSeed
-    Returns:
-        superpixels:    list of SuperpixelSeed with updated positions
-    """
-    return None
+    def assign_pixels(self, superpixels):
+        """Assigns each pixel in an image to a superpixel seed
+        Member dependencies:
+            image, depth
+        Arguments:
+            superpixels:    list of SuperpixelSeed
+        Returns:
+            pixels:    nxm array of indices, where nxm is the size of the image and
+                each element in the array represents the index of the superpixel
+                which that pixel in the image is assigned to
+        """
+        return None
 
-def calc_norms(image, depth, pixels, superpixels):
-    """Calculates the norms
-    Arguments:
-        image:  input image (nxmx3)
-        depth:  depth image (nxm)
-        pixels: array of indices (nxm) which assigns each pixel to the index in 
-            `superpixels` of the superpixel under which this pixel falls
-        superpixels:    list of SuperpixelSeed
-    Returns:
-        superpixels:    list of SuperpixelSeed with updated norms
-    """
-    return None
+    def update_seeds(self, pixels, superpixels):
+        """Updates the locations of the superpixel seeds
+        Member dependencies:
+            image, depth
+        Arguments:
+            pixels: array of indices (nxm) which assigns each pixel to the index in 
+                `superpixels` of the superpixel under which this pixel falls
+            superpixels:    list of SuperpixelSeed
+        Returns:
+            superpixels:    list of SuperpixelSeed with updated positions
+        """
+        return None
+
+    def calc_norms(self, pixels, superpixels):
+        """Calculates the norms
+        Member dependencies:
+            image, depth
+        Arguments:
+            pixels: array of indices (nxm) which assigns each pixel to the index in 
+                `superpixels` of the superpixel under which this pixel falls
+            superpixels:    list of SuperpixelSeed
+        Returns:
+            superpixels:    list of SuperpixelSeed with updated norms
+        """
+        return None
 
 if __name__ == "__main__":
     pass
--- src/test_superpixel_extraction.py
+++ src/test_superpixel_extraction.py
@@ -5,10 +5,34 @@ tests superpixel extractions
 import unittest
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from superpixel_extraction import *
 from superpixel_seed import SuperpixelSeed
 from surfel_element import SurfelElement
+
+
+expected_init_centers = [
+    [10, 10], [10, 30], [10, 50], [10, 70], [10, 90],
+    [30, 10], [30, 30], [30, 50], [30, 70], [30, 90],
+    [50, 10], [50, 30], [50, 50], [50, 70], [50, 90],
+    [70, 10], [70, 30], [70, 50], [70, 70], [70, 90],
+    [90, 10], [90, 30], [90, 50], [90, 70], [90, 90]
+]
+expected_init_intensities = [
+    [100, 0, 0], [100, 0, 0], [100, 0, 0], [100, 0, 100], [100, 0, 100],
+    [0, 100, 0], [0, 100, 0], [0, 100, 0], [100, 0, 100], [100, 0, 100],
+    [0, 0, 100], [0, 0, 100], [0, 0, 100], [100, 100, 100], [100, 100, 100],
+    [100, 100, 0], [100, 100, 0], [100, 100, 0], [100, 100, 100], [100, 100, 100],
+    [0, 100, 100], [0, 100, 100], [0, 100, 100], [100, 100, 100], [100, 100, 100]
+]
+expected_init_depths = [
+    1, 1, 1, 200, 200,
+    10, 10, 10, 200, 200,
+    50, 50, 50, 250, 250,
+    100, 100, 100, 250, 250,
+    150, 150, 150, 250, 250,
+]
 
 class TestSuperpixelExtraction(unittest.TestCase):
 
@@ -16,6 +40,10 @@ class TestSuperpixelExtraction(unittest.TestCase):
         super(TestSuperpixelExtraction, self).__init__(*args, **kwargs)
         self.image = plt.imread("test_data/test_image.png")
         self.depth = plt.imread("test_data/test_depth.png")
+        (w, h) = self.image.shape
+        camera_parameters = {'fx': 1, 'fy': 1, 'cx': w/2, 'cy': h/2}
+        self.spExtractor = SuperpixelExtraction(self.image, self.depth,
+            camera_parameters, sp_size=10)
 
     def test_extract_superpixels(self):
         pass
@@ -24,30 +52,10 @@ class TestSuperpixelExtraction(unittest.TestCase):
         """Tests initializing superpixel seeds
         """
         sp_size = 20
-        expected_centers = [
-            [10, 10], [10, 30], [10, 50], [10, 70], [10, 90],
-            [30, 10], [30, 30], [30, 50], [30, 70], [30, 90],
-            [50, 10], [50, 30], [50, 50], [50, 70], [50, 90],
-            [70, 10], [70, 30], [70, 50], [70, 70], [70, 90],
-            [90, 10], [90, 30], [90, 50], [90, 70], [90, 90]
-        ]
-        expected_intensities = [
-            [100, 0, 0], [100, 0, 0], [100, 0, 0], [100, 0, 100], [100, 0, 100],
-            [0, 100, 0], [0, 100, 0], [0, 100, 0], [100, 0, 100], [100, 0, 100],
-            [0, 0, 100], [0, 0, 100], [0, 0, 100], [100, 100, 100], [100, 100, 100],
-            [100, 100, 0], [100, 100, 0], [100, 100, 0], [100, 100, 100], [100, 100, 100],
-            [0, 100, 100], [0, 100, 100], [0, 100, 100], [100, 100, 100], [100, 100, 100]
-        ]
-        expected_depths = [
-            1, 1, 1, 200, 200,
-            10, 10, 10, 200, 200,
-            50, 50, 50, 250, 250,
-            100, 100, 100, 250, 250,
-            150, 150, 150, 250, 250,
-        ]
         passed = [False]*6
 
-        superpixels = init_seeds(self.image, self.depth, sp_size=10)
+        superpixels = self.spExtractor.init_seeds()
+
         self.assertEqual(len(superpixels), len(expected_centers),
             "didn't return the correct number of superpixels")
 
@@ -72,6 +80,25 @@ class TestSuperpixelExtraction(unittest.TestCase):
             self.assertTrue(elem, "not all superpixel centers found")
 
     def test_assign_pixels(self):
+        image = self.image
+        depth = self.depth
+
+        superpixels = []
+        for i in range(len(expected_init_centers)):
+            superpixels.append(SuperpixelSeed(
+                expected_init_centers[i][0],
+                expected_init_centers[i][1],
+                0, 0, 0, 0, 0, 0, 0, 0,
+                expected_init_depths[i],
+                expected_init_intensities[i],
+                0, 0, 0, 0
+            ))
+        superpixels = init_seeds(image, depth, sp_size=10)
+
+        expected_pixels = np.zeros((100,100), dtype=np.int32)
+        expected_pixels[0:20, 0:20] = 0
+        expected_pixels
+
         pass
 
     def test_update_seeds(self):
