2f03c35c7ea0f16ff546b2500ab2ac17294cb147
Shicong Ma <alexma3312@gmail.com>
2019-11-26 18:22:57 -0500

    Add create surfels and unittest.

--- src/test_surfel_generation.py
+++ src/test_surfel_generation.py
@@ -4,20 +4,52 @@ import unittest
 from surfel_generation import SurfelGeneration
 from superpixel_seed import SuperpixelSeed
 import numpy as np
+from mock import patch
+
+with patch.object(SurfelGeneration, "__init__", lambda x1, x2, x3, x4: None):
+    surfel_generation_instance = SurfelGeneration(None, None, None)
+
 
 class TestSurfelGeneration(unittest.TestCase):
+    # @unittest.skip("skip test_create_surfels")
     def test_create_surfels(self):
         """Test initialize Surfels from superpixels."""
         # initialize_surfels
+        x, y = 1, 1
+        size = 2
+        norm_x, norm_y, norm_z = [1,1,1]
+        position_x, position_y, position_z = [1,1,1]
+        view_cos = 0.1
+        mean_depth = 5
+        mean_intensity = 100
+        fused = False
+        stable = True
+        min_eigen_value = 10
+        max_eigen_value = 10
+        superpixel_1 = SuperpixelSeed(x, y, size, norm_x, norm_y, norm_z, position_x, position_y, position_z,
+                                      view_cos, mean_depth, mean_intensity, fused, stable, min_eigen_value, max_eigen_value)
+        superpixel_2 = SuperpixelSeed(x, y, size, norm_x, norm_y, norm_z, position_x, position_y, position_z,
+                                      view_cos, mean_depth, mean_intensity, fused, stable, min_eigen_value, max_eigen_value)
+        superpixels = [superpixel_1, superpixel_2]
+
+        surfel_generation_instance.MAX_ANGLE_COS = 0.1
+        surfel_generation_instance.all_surfels = []
+        surfel_generation_instance.fx = 1
+        surfel_generation_instance.fy = 1
+        surfel_generation_instance.VERBOSE = False
+
+        surfel_generation_instance.create_surfels(1, superpixels)
         pass
 
+    # @unittest.skip("skip test_update_surfels")
     def test_update_surfels(self):
         "Test update surfels"
-        sg = SurfelGeneration()
+        sg = surfel_generation_instance
         sp = SuperpixelSeed(0, 0, 1,
                             0, 0, 1,
                             0, 0, 1, 0, 0, 0, 0, 0, 0, 0)
-        pose0 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        pose0 = np.array([[1, 0, 0, 0], [0, 1, 0, 0],
+                          [0, 0, 1, 0], [0, 0, 0, 1]])
         sg.update_surfels([sp], pose0)
         # identical surfel should be fused
         self.assertEqual(len(sg.all_surfels), 1)
@@ -28,7 +60,8 @@ class TestSurfelGeneration(unittest.TestCase):
         sp.posi_z = 0
         sp.norm_x = 1
         sp.norm_z = 0
-        pose1 = np.array([[0, 0, -1, 0], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1]])
+        pose1 = np.array([[0, 0, -1, 0], [0, 1, 0, 0],
+                          [1, 0, 0, 0], [0, 0, 0, 1]])
         sg.update_surfels([sp], pose1)
         self.assertEqual(len(sg.all_surfels), 1)
         # surfel in same location but different norm should NOT be fused
@@ -39,6 +72,7 @@ class TestSurfelGeneration(unittest.TestCase):
         # surfel in different location and different norm should NOT be fused
         sg.update_surfels([sp], pose0)
         self.assertEqual(len(sg.all_surfels), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
