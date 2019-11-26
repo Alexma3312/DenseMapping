a8bd658b4749fa380528e0eb3c1d52bbe45bced3
Gerry Chen <gdc9@duke.edu>
2019-11-26 17:03:12 -0500

    surfel utilities and tests: transform and backprojection

--- src/surfel_element.py
+++ src/surfel_element.py
@@ -1,5 +1,8 @@
 """Surfel Element."""
 
+from typing import Tuple
+from superpixel_seed import SuperpixelSeed
+import numpy as np
 
 class SurfelElement():
     def __init__(self, px, py, pz, nx, ny, nz, size, color, weight, update_times, last_update):
@@ -20,3 +23,27 @@ class SurfelElement():
         self.weight = weight
         self.update_times = update_times
         self.last_update = last_update
+
+    def change_coordinates(self, Twc):
+        """Transforms surfel from camera pose into world coordinates
+        Arguments:
+            Twc: camera pose in world coordinates"""
+        p = np.array([self.px, self.py, self.pz, 1])
+        p = Twc.dot(p)
+        n = np.array([self.nx, self.ny, self.nz, 0])
+        n = Twc.dot(n)
+        return SurfelElement(p[0], p[1], p[2],
+                             n[0], n[1], n[2],
+                             self.size, self.color, self.weight, self.update_times,
+                             self.last_update)
+
+    def back_project(self, camera_parameters) -> Tuple[float, float]:
+        """Back projects surfel into camera image
+        Arguments:
+            camera_parameters: camera parameters as dictionary with fx, fy, cx, cy
+        Returns:
+            (x, y): surfel center position in camera image
+        """
+        x = self.px / self.pz * camera_parameters['fx'] + camera_parameters['cx']
+        y = self.py / self.pz * camera_parameters['fy'] + camera_parameters['cy']
+        return x, y
--- src/test_surfel_element.py
+++ src/test_surfel_element.py
@@ -2,7 +2,8 @@
 
 from surfel_element import SurfelElement
 import unittest
-
+from superpixel_seed import SuperpixelSeed
+import numpy as np
 
 class TestSurfelElement(unittest.TestCase):
     """Unit tests for Superpixel Seed."""
@@ -26,6 +27,41 @@ class TestSurfelElement(unittest.TestCase):
         self.assertEqual((size, color, weight, update_times, last_update), (surfel_element.size,
                                                                             surfel_element.color, surfel_element.weight, surfel_element.update_times, surfel_element.last_update))
 
+    def test_change_coordinates(self):
+        """Test coordinate change"""
+        n = np.array([1, 0, 0])
+        p = np.array([0, 0, 1])
+        Twc = np.array([[1, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        surfel = SurfelElement(p[0], p[1], p[2], n[0], n[1], n[2], 1, 1, 0, 0, 0)
+        surfel_c = surfel.change_coordinates(Twc)
+        self.assertEqual(surfel_c.px, 1)
+        self.assertEqual(surfel_c.py, 0)
+        self.assertEqual(surfel_c.pz, 1)
+        self.assertEqual(surfel_c.nx, 1)
+        self.assertEqual(surfel_c.ny, 0)
+        self.assertEqual(surfel_c.nz, 0)
+        Twc = np.array([[0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]) # CW 90 deg
+        surfel_c = surfel.change_coordinates(Twc)
+        self.assertEqual(surfel_c.px, 0)
+        self.assertEqual(surfel_c.py, 0)
+        self.assertEqual(surfel_c.pz, 1)
+        self.assertEqual(surfel_c.nx, 0)
+        self.assertEqual(surfel_c.ny, -1)
+        self.assertEqual(surfel_c.nz, 0)
+
+    def test_back_project(self):
+        """Test back projection of surfel into superpixel"""
+        p = np.array([1, 2, 1])
+        camera_parameters = {'fx': 100, 'fy': 100,
+                             'cx': 0, 'cy': 0}
+        surf = SurfelElement(p[0], p[1], p[2], 0, 0, 0, 0, 0, 0, 0, 0)
+        x, y = surf.back_project(camera_parameters)
+        self.assertEqual(x, 100)
+        self.assertEqual(y, 200)
+        surf.pz = 2
+        x, y = surf.back_project(camera_parameters)
+        self.assertEqual(x, 50)
+        self.assertEqual(y, 100)
 
 if __name__ == "__main__":
     unittest.main()
