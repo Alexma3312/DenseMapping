7452c9cb2f405b642bf450620ab9e7ae598775f9
Gerry Chen <gdc9@duke.edu>
2019-11-26 17:24:49 -0500

    create surfel_generation class

--- src/surfel_generation.py
+++ src/surfel_generation.py
@@ -1,13 +1,27 @@
 """Surfel Generation"""
 """Surfel Generation."""
 from surfel_element import SurfelElement
+from superpixel_seed import SuperpixelSeed
+from typing import List
 
-def initialize_surfels(reference_frame_index, pose):
-    """Initialize the surfels for given frame
-    Arguments:
-        reference_frame_index: the index of the reference frame
-        pose: a 4x4 matrix (4x4 numpy array)
-    Returns:
-        new_surfels: a list of surfel elements
-    """
-    pass
+class SurfelGeneration():
+    
+    def __init__(self):
+        self.all_surfels = []
+    
+    def create_surfels(self, superpixels -> List[SuperpixelSeed]) -> List[SurfelElement]:
+        """Create surfels from superpixels for given frame
+        Arguments:
+            superpixels: list of SuperpixelSeed
+        Returns:
+            surfels: a list of surfel elements
+        """
+        pass
+    
+    def update_surfels(self, superpixels -> List[SuperpixelSeed], pose) -> None:
+        """Update internal global list of surfels using the superpixels from one frame
+        Arguments:
+            superpixels: list of SuperpixelSeed
+            pose: camera pose in world coordinates
+        """
+        pass
--- src/test_surfel_generation.py
+++ src/test_surfel_generation.py
@@ -5,11 +5,14 @@ from surfel_generation import initialize_surfels
 
 
 class TestSurfelGeneration(unittest.TestCase):
-    def test_initialize_surfels(self):
-        """Initialize Surfels."""
+    def test_create_surfels(self):
+        """Test initialize Surfels from superpixels."""
         # initialize_surfels
         pass
 
+    def test_update_surfels(self):
+        "Test update surfels"
+        pass
 
 if __name__ == '__main__':
     unittest.main()
