effde2d037d9df0329b7d6014c8845754d91e640
Gerry Chen <gdc9@duke.edu>
2019-11-26 19:28:34 -0500

    update surfels should be working as long as is_fuseable and fuse_surfel both work.

--- src/surfel_generation.py
+++ src/surfel_generation.py
@@ -18,28 +18,25 @@ class SurfelGeneration():
     def get_weight(self, depth):
         return min(1.0 / depth / depth, 1.0)
 
-    def create_surfels(self, frame_idx, superpixels: List[SuperpixelSeed]) -> List[SurfelElement]:
+    def create_surfels(self, frame_idx, superpixels: List[SuperpixelSeed], pose) -> List[SurfelElement]:
         """Create surfels from superpixels for given frame
         Arguments:
+            frame_idx: index of current frame
             superpixels: list of SuperpixelSeed
+            pose: pose of camera in current frame
         Returns:
             surfels: a list of surfel elements
         """
         surfels = []
         for superpixel in superpixels:
-            if superpixel.mean_depth == 0:
-                continue
-            if superpixel.fused:
-                continue
-            if superpixel.view_cos < self.MAX_ANGLE_COS:
-                continue
             px = superpixel.posi_x
             py = superpixel.posi_y
             pz = superpixel.posi_z
             nx = superpixel.norm_x
             ny = superpixel.norm_y
             nz = superpixel.norm_z
-            camera_f = (fabs(self.fx) + fabs(self.fy)) / 2.0
+            camera_f = (fabs(self.camera_parameters['fx']) +
+                        fabs(self.camera_parameters['fy'])) / 2.0
             new_size = superpixel.size * \
                 fabs(superpixel.mean_depth / (camera_f * superpixel.view_cos))
             if(new_size > 0.1 and self.VERBOSE):
@@ -54,13 +51,18 @@ class SurfelGeneration():
             surfels.append(surfel)
         return surfels
 
-    def update_surfels(self, superpixels: List[SuperpixelSeed], pose) -> None:
+    def update_surfels(self, frame_idx, superpixels: List[SuperpixelSeed], pose) -> None:
         """Update internal global list of surfels using the superpixels from one frame
         Arguments:
+            frame_idx: index of current frame, for storing when a surfel last got updated
             superpixels: list of SuperpixelSeed
             pose: camera pose in world coordinates
         """
-        new_surfels = self.create_surfels(superpixels)
+        for sp in superpixels:
+            if (sp.mean_depth == 0) or (sp.fused) or (sp.view_cos < self.MAX_ANGLE_COS):
+                superpixels.remove(sp)
+        new_surfels = self.create_surfels(frame_idx, superpixels, pose)
+        print(new_surfels)
         projected_locs = np.zeros((len(self.all_surfels), 2))
         for i in range(len(self.all_surfels)):
             projected_locs[i, :] = \
@@ -70,7 +72,8 @@ class SurfelGeneration():
             dists2 = np.sum(np.square(sp_pos - projected_locs), axis=1)
             candidate_surfels = dists2 < sp.size
             did_fuse = False
-            for surfel in candidate_surfels:
+            for surfel_i in np.argwhere(candidate_surfels):
+                surfel = new_surfels[surfel_i[i]]
                 if surfel.is_fuseable(new_surfels[i]):
                     surfel.fuse_surfel(new_surfels[i])
                     did_fuse = True
